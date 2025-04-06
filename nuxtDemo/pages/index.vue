3adb328d5df5878a6a3dceee7a49d3ea4d4491b3
annamazurok <mazurokanna25@gmail.com>
2025-04-07 00:53:53 +0300

    feat: added page

---
+++ nuxtDemo/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'Список продуктів'
+})
+</script>
+
+<template>
+  <div>
+    <h1>Список продуктів</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
